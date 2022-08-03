317673520cf6d739529b36fb8ee877838da5ee84
Maxi-raph <102929251+Maxi-raph@users.noreply.github.com>
2022-08-03 11:19:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ button.addEventListener('click',() => {
        //Creating the checbox container //
        const checkBoxContainer = document.createElement('div')
        checkBoxContainer.style.display = 'flex'
-       checkBoxContainer.style.width = '15%'
-       checkBoxContainer.style.justifyContent = 'space-between'
+       checkBoxContainer.style.justifyContent = 'start'
        // Creating the checkboxes //
        const firstCheckBox = document.createElement('input')
        firstCheckBox.setAttribute('type','checkbox')
@@ -27,29 +26,33 @@ button.addEventListener('click',() => {
        checkBoxContainer.appendChild(secondCheckBox)
        // Looping through the array, creating listItem for each array and putting it in the todo_list //
         arrOfObj.forEach(arr => {
-            const listItem = document.createElement('li')
+            const listItem = document.createElement('p')
             const text = document.createTextNode(arr)
             listItem.classList.add('list')
             listItem.appendChild(text)
-            listItem.appendChild(checkBoxContainer)
-            todoList.appendChild(listItem)
+            const listItemContainer = document.createElement('div')
+            listItemContainer.classList.add('list_container')
+            listItemContainer.appendChild(listItem)
+                    listItemContainer.appendChild(checkBoxContainer)
+            todoList.appendChild(listItemContainer)
         // Adding an event to the first checkbox to put a line through on the listItem //  
             firstCheckBox.addEventListener('click',() => {
-                listItem.classList.toggle('checked')
+               listItemContainer.classList.toggle('checked')
+              
                 firstCheckBox.style.display = 'none'
                 checkBoxContainer.style.justifyContent = 'flex-end'
                })
-        // Adding an event to the second checkbox to delete the listItem // 
+        // Adding an event to the second checkbox to delete the listItem container // 
                secondCheckBox.addEventListener('click',() => {
               if(firstCheckBox.style.display !== 'none'){
-                  Return
+                  return
              }
               else if(firstCheckBox.style.display === 'none'){
-                listItem.style.transform = 'scale(0)'
-                if (listItem.style.transform == 'scale(0)') {
+                listItemContainer.style.transform = 'scale(0)'
+                if (listItemContainer.style.transform == 'scale(0)') {
                      new Promise((resolve) =>{
                         setTimeout( () =>{
-                            resolve(todoList.removeChild(listItem))
+                            resolve(todoList.removeChild(listItemContainer))
                         },200)
                      })
                }
